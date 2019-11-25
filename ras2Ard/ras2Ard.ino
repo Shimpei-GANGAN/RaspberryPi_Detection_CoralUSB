aa0cd938436de3cc15ed2045451a974ad73a9705
Shimpei Iwamaru <gangan.education@gmail.com>
2019-11-25 20:30:59 +0900

    .inoの変更

---
+++ ras2Ard/ras2Ard.ino
@@ -0,0 +1,23 @@
+#define LED_PIN1 2
+byte val = 0;
+
+void setup() {
+  pinMode(LED_PIN1, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if ( Serial.available() > 0 ) {
+    val = Serial.read();
+  }
+  //  条件分岐
+  if ( val == '1') {
+    digitalWrite(LED_PIN1, HIGH);
+    Serial.print("i received.");
+    Serial.println("1");
+  }
+  else if( val == '0'){
+    digitalWrite(LED_PIN1, LOW);
+    Serial.println(val);
+  }
+}
