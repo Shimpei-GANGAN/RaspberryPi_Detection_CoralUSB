7f782f7959b29f9ac59c4fec0e717c8eacfc476a
Shimpei-GANGAN <gangan.education@gmail.com>
2019-11-28 14:04:32 +0900

    fixed serial-asyncio

--- test_pyserial-asyncio.py
+++ test_pyserial-asyncio.py
@@ -2,56 +2,66 @@
 #   coding:utf-8
 #------------------------------------------------------------
 #   Updata History
-#   November  27  11:00, 2019 (Wed)
+#   November  28  07:00, 2019 (Thu)
 #------------------------------------------------------------
 #   Arduinoとの非同期通信test
 #------------------------------------------------------------
 
 import asyncio
 import serial_asyncio
-import sys
-from random import randint
 
 """
     Raspberry Pi to Arduino
 """
 class Output(asyncio.Protocol):
+    def __init__(self):
+        super().__init__()
+        self._transport = None
+
     #  Called when creating a connection
     def connection_made(self, transport):
-        self.transport = transport
-        print("port opened", transport)
-        transport.serial.rts = False
-        transport.write(b"1;")
+        self._transport = transport
+        print("port opened", self._transport)
+        self._transport.serial.rts = False
+        self._transport.write(b"q;\n")
 
     #  When data is received
     def data_received(self, data):
-        print("data received:", data)
+        print("data received", data)
+        if b"\n" in data:
+            self._transport.close()
+
+        """
         if data == "finished":
+            print("data received:", data)
             self.transport.close()
-        self.transport.write(b"0;")
-        
+        elif data == "1":
+            self.transport.write(b"0;")
+        elif data == "0":
+            self.transport.write(b"1;")
+        """
+
     #  When a connection is lost
     def connection_lost(self, exc):
         print("port closed")
-        asyncio.get_event_loop().stop()
+        self._transport.loop.stop()
 
 #  main function
-def main():
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     coro = serial_asyncio.create_serial_connection(
         loop, Output, 
         "/dev/ttyACM0",
         baudrate=9600)
-    server = loop.run_until_complete(coro)
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
+    #    timeout=0.1)
+    loop.run_until_complete(coro)
+    #try:
+    loop.run_forever()
+    #except KeyboardInterrupt:
+    #    pass
 
     #  Close the server
-    server.close()
-    loop.run_until_complete(server.wait_closed())
+    #server.close()
+    #loop.run_until_complete(server.wait_closed())
     loop.close()
 
-if __name__ == "__main__":
-    main()
